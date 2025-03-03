e3c0868dc68249a16be19089c64f0f06041839da
macos <badboycxcc@gmail.com>
2025-03-03 20:06:26 +0800

    2025-03-03:20:06:26-mac

---
+++ push.sh
@@ -0,0 +1,4 @@
+git status
+git add .
+git commit -m `date +%Y-%m-%d:%H:%M:%S`-mac
+git push origin main
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,8 +1,4 @@
 pub const PUB_KEY: &str = "-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCG96JU8HQUMI+KP/Cik9zzAavP
-++Bht+wF++8+xf350Jrh72ozIg2CDmf6MUiLc6WHW7Dralj5ELi/smlaftVdGFQM
-LTYiIrLZ/6lpp1aIICVzo6p5s5/VRp7W6biqRekPFLBqajdjKPVKFF1y56TzDHT8
-lPEVpCPD5xRBvnbmdQIDAQAB
 -----END PUBLIC KEY-----";
 pub const USER_AGENT: &str =
     "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; Avant Browser)";
